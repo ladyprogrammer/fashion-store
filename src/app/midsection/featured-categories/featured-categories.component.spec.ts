c7cb6115a06add492d1cd3330b12b91104f25e83
Gem <gem@ladyprogrammer.net>
2022-11-28 00:18:39 +0800

    featured categories layout

--- src/app/midsection/featured-categories/featured-categories.component.spec.ts
+++ src/app/midsection/featured-categories/featured-categories.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { FeaturedCategoriesComponent } from './featured-categories.component';
 
@@ -17,7 +18,26 @@ describe('FeaturedCategoriesComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have two categories', () => {
+    const NO_OF_FEATURED_CATEGORIES = 2;
+    const featuredCategories = fixture.debugElement.queryAll(By.css('[data-test="featured-category"]'));
+    expect(featuredCategories.length).toEqual(NO_OF_FEATURED_CATEGORIES);
+  });
+
+  it('should have title, content and button per category', () => {
+    const NO_OF_ITEMS = 2;
+    const featuredCategories = fixture.debugElement.queryAll(By.css('[data-test="featured-category"]'));
+    const titles = fixture.debugElement.queryAll(By.css('[data-test="title"]'));
+    const content = fixture.debugElement.queryAll(By.css('[data-test="content"]'));
+    const buttons = fixture.debugElement.queryAll(By.css('[data-test="button"]'));
+    expect(featuredCategories.length).toEqual(NO_OF_ITEMS);
+    expect(titles.length).toEqual(NO_OF_ITEMS);
+    expect(content.length).toEqual(NO_OF_ITEMS);
+    expect(buttons.length).toEqual(NO_OF_ITEMS);
+  });
+
+  it('should use right HTML elements', () => {
+    const buttons = fixture.debugElement.queryAll(By.css('[data-test="button"]'));
+    buttons.forEach((button) => expect(button.nativeElement).toBeInstanceOf(HTMLButtonElement));
   });
 });
