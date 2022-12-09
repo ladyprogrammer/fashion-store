6188656613f57e207abb43d84df290491d3f99b3
Gem <gem@ladyprogrammer.net>
2022-12-09 18:53:43 +0800

    detailed css for footer

--- src/app/footer/address/address.component.ts
+++ src/app/footer/address/address.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['../footer.component.scss', './address.component.scss']
 })
 export class AddressComponent {
-
+  title = 'About MeStore';
+  description = 'This is a demo of a site created in Angular 14. TDD was practiced for as much as possible.';
+  location = '0123 Main Road, Your City, NY 123456';
+  phone = '(000) 2345-6789';
+  email = 'info@samplesite';
 }
