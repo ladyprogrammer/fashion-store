d7dc6ce1836590ce70092dca3499783dcf9760c3
Gem <gem@ladyprogrammer.net>
2022-12-14 11:41:10 +0800

    refactor hero UI

--- src/app/home/hero/hero.component.spec.ts
+++ src/app/home/hero/hero.component.spec.ts
@@ -1,4 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { HeroComponent } from './hero.component';
@@ -6,30 +8,56 @@ import { HeroComponent } from './hero.component';
 describe('HeroComponent', () => {
   let component: HeroComponent;
   let fixture: ComponentFixture<HeroComponent>;
+  let location: Location;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeroComponent ],
-      imports: [ RouterTestingModule ]
+      declarations: [ HeroComponent, MockComponent ],
+      imports: [ 
+        RouterTestingModule.withRoutes([
+          { path: 'shop', component: MockComponent },
+        ]),
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(HeroComponent);
     component = fixture.componentInstance;
+    location = TestBed.inject(Location);
     fixture.detectChanges();
   });
 
-  it('should have the title, content and shop now button', () => {
-    const title = fixture.nativeElement.querySelector('[data-test="title"]');
-    const content = fixture.nativeElement.querySelector('[data-test="content"]');
-    const button = fixture.nativeElement.querySelector('[data-test="shop-now-button"]');
-    expect(title).toBeTruthy();
-    expect(content).toBeTruthy();
-    expect(button).toBeTruthy();
+  describe('content', () => {
+    it('should have the title', () => {
+      const title = fixture.nativeElement.querySelector('[data-test="title"]');
+      expect(title).toBeTruthy();
+    });
+  
+    it('should have content', () => {
+      const content = fixture.nativeElement.querySelector('[data-test="description"]');
+      expect(content).toBeTruthy();
+    });
+  });
+  
+
+  describe('shop button', () => {
+    it('should have the shop now button', () => {
+      const button = fixture.nativeElement.querySelector('[data-test="shop-now-button"]');
+      expect(button).toBeTruthy();
+      expect(button).toBeInstanceOf(HTMLButtonElement);
+    });
+  
+    it('should go to the shop page when clicked', async () => {
+      const button = fixture.nativeElement.querySelector('[data-test="shop-now-button"]') as HTMLButtonElement;
+      await button.click();
+      expect(location.path()).toBe('/shop');
+    });
   });
 
-  it('should follow semantic HTML', () => {
-    const button = fixture.nativeElement.querySelector('[data-test="shop-now-button"]');
-    expect(button).toBeInstanceOf(HTMLButtonElement);
-  });
 });
+
+
+@Component({
+  template: '',
+})
+export class MockComponent {}
--- src/app/home/hero/hero.component.ts
+++ src/app/home/hero/hero.component.ts
@@ -1,15 +1,26 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'section-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss'],
 })
-export class HeroComponent implements OnInit {
+export class HeroComponent {
+  private _titleLines: readonly string[];
+  private _content: string;
 
-  constructor() { }
+  constructor() {
+    this._titleLines = ['Fashion', 'Carnival', '2022'];
+    this._content = `This is a demo site created in Angular. This has just been created recently and is still 
+      a work in progress - you will see some that are not working as expected. Please feel free to shop around.`;
+  }
 
-  ngOnInit(): void {
+  get titleLines() {
+    return this._titleLines;
+  }
+
+  get content() {
+    return this._content;
   }
 
 }
